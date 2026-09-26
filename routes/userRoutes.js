f07c731aec8d70d38fd14a71d52a16db541b2b29
Alia Qureshi <aliaqureshi@gmail.com>
2026-09-25 21:34:43 -0400

    Add rate limiting to authentication endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const {
   signup,
   login
@@ -9,9 +10,22 @@ const {
   loginValidator
 } = require("../validators/userValidator");
 
+const { authLimiter } = require("../middleware/rateLimiter");
+
 const router = express.Router();
 
-router.post("/signup", signupValidator, signup);
-router.post("/login", loginValidator, login);
+router.post(
+  "/signup",
+  authLimiter,
+  signupValidator,
+  signup
+);
+
+router.post(
+  "/login",
+  authLimiter,
+  loginValidator,
+  login
+);
 
 module.exports = router;
